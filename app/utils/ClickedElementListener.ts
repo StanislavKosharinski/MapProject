4e078e168f996610413def28ad3154e30bde4278
strukov <vadim.strukov@gmail.com>
2016-08-02 11:32:43 +0200

    ClickedElementListener fix

--- app/utils/ClickedElementListener.ts
+++ app/utils/ClickedElementListener.ts
@@ -2,10 +2,12 @@
  * Created by strukov on 7/25/16.
  */
 export abstract class ClickedElementListener{
-
+    protected target:any;
+    protected idAttr:any;
+    
     public getClickedElementId(event: any){
-        var target = event.target || event.srcElement || event.currentTarget;
-        var idAttr = target.attributes.id;
-        return idAttr.nodeValue;
+        this.target = event.target || event.srcElement || event.currentTarget;
+        this.idAttr = this.target.attributes.id;
+        return this.idAttr.nodeValue;
     }
 }
