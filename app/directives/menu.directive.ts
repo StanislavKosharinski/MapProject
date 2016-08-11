56b2654136d051c19b929d26f36d05a8a0fe630d
vadimstrukov <vadim.strukov@gmail.com>
2016-08-11 15:39:18 +0200

    removed modal

--- app/directives/menu.directive.ts
+++ app/directives/menu.directive.ts
@@ -7,11 +7,8 @@ import {HTTP_PROVIDERS} from "@angular/http";
 import {SlopeService} from "../service/SlopeService";
 import {LiftService} from "../service/LiftService";
 import {Lift} from "../domain/Lift";
-import {ModalDirective} from "./modal.directive";
 import {MapComponent} from "../map.component";
-import {ClickedElementListener} from "../utils/ClickedElementListener";
 import {ItemType} from "../enums/ItemType";
-import {TABS_DIRECTIVES} from "./tabs/index";
 import {TabDirective} from "./tabs/tab.directive";
 import {TabsDirective} from "./tabs/tabs.directive";
 import {TabType} from "../enums/TabType";
@@ -21,12 +18,12 @@ import {TabType} from "../enums/TabType";
     templateUrl: 'app/blocks/menu_left.html',
     styleUrls: ['app/blocks/menu_left_style.css'],
     providers: [SlopeService, LiftService, HTTP_PROVIDERS],
-    directives:[TabDirective, TabsDirective, ModalDirective]
+    directives:[TabDirective, TabsDirective]
 })
 
-export class MenuDirective implements OnInit{
+export class MenuDirective implements OnInit {
 
-    @ViewChild('tabs') tabs :TabsDirective;
+    @ViewChild('tabs') tabs: TabsDirective;
 
     myLifts: Array<Lift>;
     mySlopes: Array<Slope>;
@@ -34,87 +31,40 @@ export class MenuDirective implements OnInit{
     myLift: Lift;
     mySlope: Slope;
 
-    ids:Array<string>;
-    selectedItem:any;
+    ids: Array<string>;
+    selectedItem: any;
+    isExpanded: boolean = false;
+    tempItem: any;
 
     public itemType = ItemType;
 
-    constructor(private slopeService:SlopeService, private liftService:LiftService,
-                @Inject(forwardRef(() => MapComponent)) private map:MapComponent){
+    constructor(private slopeService: SlopeService, private liftService: LiftService,
+                @Inject(forwardRef(() => MapComponent)) private map: MapComponent) {
     }
 
     ngOnInit() {
-        this.getMenuWidth();
         this.getSpecificIds();
         this.getSpecificItems();
     }
 
-    getSpecificIds(){
+    getSpecificIds() {
         this.ids = this.map.getIdsFromMap();
     }
 
-    getSpecificItems(){
+    getSpecificItems() {
         this.slopeService.getSpecificSlopes(this.ids).subscribe(data => this.mySlopes = data);
-        this.liftService.getSpecificLifts(this.ids).subscribe(data => this.myLifts= data);
+        this.liftService.getSpecificLifts(this.ids).subscribe(data => this.myLifts = data);
     }
 
-
-    isSelectedItem(item:any){
-        return this.selectedItem===item;
+    isSelectedItem(item: any) {
+        return this.selectedItem === item;
     }
 
-    getExpanded(item:any){
+    getExpanded(item: any) {
         this.selectedItem = item;
     }
 
-    selectTabByTitle(tabType:TabType){
+    selectTabByTitle(tabType: TabType) {
         this.tabs.getAllTabs().filter(tab=>tab.title === tabType.title)[0].active = true;
     }
-
-    /**
-     * @deprecated Modal not supported any more, use left menu instead
-     */
-    getMenuWidth(){
-        return document.getElementById('menu').offsetWidth;
-    }
-
-    /**
-     * @deprecated Modal not supported any more, use left menu instead
-     */
-    private getModal(event: MouseEvent){
-        var menuItem = document.getElementById(ClickedElementListener.getClickedElementId(event));
-        for(var i = 0; i < this.map.getIdsFromMap().length; i++){
-            if(this.map.getIdsFromMap()[i] == menuItem.id) {
-                var mapItem = this.map.getMapDocument().item(i);
-                this.map.modal.elementX = this.getMapItemPosition(mapItem, ItemPosition.LEFT);
-                this.map.modal.elementY = this.getMapItemPosition(mapItem, ItemPosition.TOP) ;
-            }
-        }
-        return menuItem;
-    }
-
-    /**
-     * @deprecated Modal not supported any more, use left menu instead
-     */
-    private getMapItemPosition(mapItem:any, position:ItemPosition){
-        let coordinates = mapItem.getAttribute('d');
-        switch (position){
-            case ItemPosition.TOP:
-                let mapItemTopPattern = /(?=.*),.*/;
-                let mapItemTopPattern2 = /[^,]+/;
-                let mapItemTopFirstRegExp = coordinates.match(mapItemTopPattern).toString();
-                return mapItemTopFirstRegExp.match(mapItemTopPattern2);
-            case ItemPosition.LEFT:
-                let mapItemLeftPattern = /\S+(?=,)/;
-                return coordinates.match(mapItemLeftPattern);
-        }
-    }
-}
-
-/**
- * @deprecated Modal not supported any more, use left menu instead
- */
-enum ItemPosition{
-    TOP,
-    LEFT
 }
--- app/map.component.ts
+++ app/map.component.ts
@@ -2,7 +2,6 @@
  * Created by strukov on 7/19/16.
  */
 import {Component, ViewChild, OnInit} from "@angular/core";
-import {ModalDirective} from "./directives/modal.directive";
 import {MenuDirective} from "./directives/menu.directive";
 import {ClickedElementListener} from "./utils/ClickedElementListener";
 import {ItemType} from "./enums/ItemType";
@@ -13,13 +12,11 @@ declare var svgPanZoom:any;
   selector: 'map-lines',
   templateUrl:'app/blocks/map_lines.html',
   styleUrls: ['app/blocks/map_lines_style.css'],
-  directives: [ModalDirective, MenuDirective]
+  directives: [MenuDirective]
 })
 
 export class MapComponent  implements OnInit{
 
-
-    @ViewChild('modal') modal :ModalDirective;
     @ViewChild('left_menu') menu :MenuDirective;
     public itemType = ItemType;
 
