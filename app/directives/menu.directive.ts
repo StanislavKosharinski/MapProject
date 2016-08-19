a531d0c9cd06e7ca9b69427aea1ccf0e624aa145
vadimstrukov <vadim.strukov@gmail.com>
2016-08-19 14:10:10 +0200

    small fix with expanding and collapsing

--- app/directives/menu.directive.ts
+++ app/directives/menu.directive.ts
@@ -40,6 +40,7 @@ export class MenuDirective implements OnInit {
     //Filling objects from array
     setItemById(id:string, item:ItemType){
         this.isOpen = true;
+        this.isExpanded = true;
         if(document.getElementById("menu"))
             this.expandMenu(document.getElementById("menu"), document.getElementById("left_arrow"));
         switch (item){
